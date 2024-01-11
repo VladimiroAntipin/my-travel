b7e0a7bafa0eede4ce2fc500fce52ee2bcc18a51
Vladimiro Antipin <antipinvladimiro@yandex.ru>
2024-01-11 16:10:39 +0300

    added favorites page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,26 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings"
+import ClientOnly from "../components/ClientOnly"
+import EmptyState from "../components/EmptyState"
+import FavoritesClient from "./FavoritesClient";
+
+const ListingPage = async () => {
+    const listings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
+
+    if(listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState title="No favorites found" subtitle="It seems you don't have favorites listings"/>
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <FavoritesClient listings={listings} currentUser={currentUser}/>
+        </ClientOnly>
+    )
+}
+
+export default ListingPage;
